af5aea6c9a639e282f42fbed4d701ef138913f78
ALEXSTR19 <alex129t@outlook.com>
2024-10-25 09:47:11 -0500

    Logro: se crea componente busqueda en archivo clininc y su contenido

--- src/app/business/archivo-clinic/components/busqueda/busqueda.component.ts
+++ src/app/business/archivo-clinic/components/busqueda/busqueda.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-busqueda',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './busqueda.component.html',
   styleUrl: './busqueda.component.css'
 })
 export default class BusquedaComponent {
+  formBusqueda: FormGroup = new FormGroup
+
+  ({
+    expediente: new FormControl('', Validators.required),
+     nombre: new FormControl('', Validators.required),
+     
+   });
+
+  onSubmit() {
+    if (this.formBusqueda.valid) {
+      console.log('Formulario válido, datos enviados:', this.formBusqueda.value);
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
+
 
 }
