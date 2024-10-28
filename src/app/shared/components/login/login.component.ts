378634b9602e59f6fda36c3fa87052c1f9bc4455
M.S.C. Adolfo Simbron <simbron2803@gmail.com>
2024-10-28 09:15:06 -0600

    Logro : se configura login

---
+++ src/app/shared/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule,ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export default class LoginComponent implements
+  OnInit {
+    loginForm!: FormGroup;
+  
+    constructor(private fb: FormBuilder) { }
+  
+    ngOnInit(): void {
+      this.loginForm = this.fb.group({   
+  
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+      });
+    }
+  
+    onSubmit(): void {
+      if (this.loginForm.valid)   
+   {
+        // Aquí va la lógica para enviar los datos al servidor
+        console.log(this.loginForm.value);
+      }
+    }
+
+}
